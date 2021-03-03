7ad44cfbaf9f7fff704a13353264dfb30e9d85f9
Christian-Schneider-Davis <schneiderdavis@aol.com>
2021-03-03 02:12:45 -0600

    nmp/react errors. Scrapping and starting over

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+import "./style.css";
+
+
+
+function App() {
+    return (
+        <div className="App">
+            <form>
+                <input
+                maxLength= "15"
+                placeholder= "Recipe Name"
+                
+            </form>
+        </div>
+    )
+}
+
+export default App;
